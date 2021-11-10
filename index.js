c21de3b0d44f1321461ac744f393138ac75e8a6a
Amber <martinez.amber1028@gmail.com>
2021-11-09 18:39:35 -0800

    Assignment Complete.

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
+//=> Instruction #1
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+
+multiply;
+
+//=> Instruction #2
+let random = 0;
+
+++random;
+
+//=> Instruction #3
+let num3 = 10;
+let num4 = 6;
+
+let mod = num3 % num4;
+
+mod;
+
+//=> Instruction #4
+let max = Math.max(10, 20, -5, 7);
+
+max;
